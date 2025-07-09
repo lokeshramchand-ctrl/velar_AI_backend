2d111159b2804a0b158e8a18b223b46ad92bff7e
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2025-07-09 18:08:04 +0530

    Redresh

--- predict_api.py
+++ predict_api.py
@@ -1,13 +1,18 @@
+import os
 from flask import Flask, request, jsonify
 import joblib  
 import sklearn  
 import numpy as np
+from flask_cors import CORS
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(BASE_DIR, "model", "category_model.pkl")
+vectorizer_path = os.path.join(BASE_DIR, "model", "vectorizer.pkl")
 
-model = joblib.load("model/category_model.pkl")
-vectorizer = joblib.load("model/vectorizer.pkl")  
+model = joblib.load(model_path)
+vectorizer = joblib.load(vectorizer_path)
 
 app = Flask(__name__)
-
+CORS(app)
 @app.route("/api/predict", methods=["POST"])
 def predict():
     data = request.json
@@ -23,4 +28,5 @@ def predict():
     return jsonify({"category": prediction})
 
 if __name__ == "__main__":
+  
     app.run(host="0.0.0.0", port=5000)
